01ac430fa856e412740016f54e22725fa01681ab
DrChipmunk <dangermunkdelta@gmail.com>
2026-02-08 22:02:04 -0500

    Play rates: Feb 2026
    Signed-off-by: DrChipmunk <dangermunkdelta@gmail.com>

--- scripts/play_rates.py
+++ scripts/play_rates.py
@@ -19,7 +19,7 @@ def get_play_rates():
 				exists = True
 			if i > 21 and i < 26:
 				exists = True
-			if i == 26 and j <= 1:
+			if i == 26 and j <= 2:
 				exists = True
 			if i == 22 and j == 4:
 				exists = False
